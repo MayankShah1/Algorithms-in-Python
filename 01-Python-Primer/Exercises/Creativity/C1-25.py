37854848ee8d4982c0a0e6495f80041efe07862a
MayankShah1 <mayankshah11996@gmail.com>
2017-06-20 16:38:44 +0530

    Minor improvement

--- 01-Python-Primer/Exercises/Creativity/C1-25.py
+++ 01-Python-Primer/Exercises/Creativity/C1-25.py
@@ -1,10 +1,10 @@
 def remove_punctuations(input_string):
     punctuation_marks = [',','\'','.','?','!']
-    ans = ''
+    ans = []
     for char in input_string:
         if char not in punctuation_marks:
-            ans += char
-    return ans
+            ans.append(char)
+    return ''.join(ans)
 
 print(remove_punctuations('Let\'s try, Mike.'))
 
